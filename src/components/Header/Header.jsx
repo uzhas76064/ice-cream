d117281793c8dfdf7f6f44d385d5050d41c85bff
Maksim <uzhas76064@gmail.com>
2024-10-15 14:46:11 +0300

    added styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,12 @@
 import logo from '../../img/logo.svg';
+import styles from './Header.module.css';
 
 const Header = props => {
     return (
         <header className="header">
-            <div className="container">
+            <div className={styles.container}>
                 <a href="#" className="logo">
-                    <img src={logo} alt=""/>
+                    <img width={152} height={32} src={logo} alt=""/>
                 </a>
                 <div className="cart">Корзина</div>
             </div>
