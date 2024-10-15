7bafbc89501400dbad35157a5b6737437f16b24a
Maksim <uzhas76064@gmail.com>
2024-10-15 14:31:18 +0300

    added styles

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,18 +1,22 @@
 import logo from '../../img/logo.svg';
+import Contacts from "../Contacts/Contacts.jsx";
+import PropTypes from "prop-types";
 
 const Footer = () => {
     return (
         <footer className="footer">
             <div className="container">
                 <a href={logo} className="logo"></a>
-                <address className="contacts">
-                    <p className="contacts__name"></p>
-                    <a  className="contacts__phone" href="tel:+79912365058">Номер для связи: +7 (991) 236-50-58</a>
-                    <a href="mailto: uzhas76064@gmail.com">Почта для связи: uzhas76064@gmail.com</a>
-                </address>
+                <Contacts email="mailto: uzhas76064@gmail.com"
+                          name="Хватов Максим"
+                          phone="tel:+79912365058"/>
             </div>
         </footer>
     )
+}
+
+Footer.propTypes = {
+    logo: PropTypes.string,
 }
 
 export default Footer;
