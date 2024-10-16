662348c8dd8598dbc416ed8766565fe6d8199daa
Maksim <uzhas76064@gmail.com>
2024-10-16 12:02:35 +0300

    подключены глобальные стили

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header/Header.jsx";
 import Catalog from "./components/Catalog/Catalog.jsx";
 import Footer from "./components/Footer/Footer.jsx";
+import "./global.css";
 
 function App() {
 
